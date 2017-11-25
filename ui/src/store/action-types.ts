b32570af71d801b8e29c3ab091e95a686ab02a6c
Manabu Matsuzaki <matsumana@gmail.com>
2017-11-25 16:44:51 +0900

    delete investigation codes

--- ui/src/store/action-types.ts
+++ ui/src/store/action-types.ts
@@ -1,27 +1,9 @@
-// sandbox
-const INPUT_NAME = 'INPUT_NAME';
-const INCREMENT = 'INCREMENT';
-const DECREMENT = 'DECREMENT';
-const HTTP_POST = 'HTTP_POST';
-const WS_SEND = 'WS_SEND';
-
-// App
 const INPUT_SQL = 'INPUT_SQL';
 const SUBMIT = 'SUBMIT';
 const WS_ON_MESSAGE = 'WS_ON_MESSAGE';
-// const CANCEL = 'CANCEL';
 
 export const ACTION = {
-  // sandbox
-  INPUT_NAME,
-  INCREMENT,
-  DECREMENT,
-  HTTP_POST,
-  WS_SEND,
-
-  // App
   INPUT_SQL,
   SUBMIT,
   WS_ON_MESSAGE,
-  // CANCEL,
 };
--- ui/src/store/mutations.ts
+++ ui/src/store/mutations.ts
@@ -8,32 +8,9 @@ import { ResponseTable } from './model/ResponseTable';
 import { ResponseTableRow } from './model/ResponseTableRow';
 
 const mutations = <MutationTree<State>> {
-  // sandbox
-  [MUTATION.INPUT_NAME](state: State, userName: string) {
-    state.userName = userName;
-  },
-  [MUTATION.INCREMENT](state: State) {
-    state.enthusiasm += 1;
-  },
-  [MUTATION.DECREMENT](state: State) {
-    if (state.enthusiasm > 1) {
-      state.enthusiasm -= 1;
-    }
-  },
-  [MUTATION.HTTP_POST](state: State, value: string) {
-    state.postResponse = value;
-  },
-  [MUTATION.WS_RECEIVE](state: State, value: string) {
-    state.wsResponse = value;
-  },
-
-  // App
   [MUTATION.INPUT_SQL](state: State, sql: string) {
     state.sql = sql;
   },
-  // [MUTATION.CANCEL](state: State, sql: string) {
-  //   state.sql = sql;
-  // },
   [MUTATION.SUBMIT](state: State, response: ResponseBase) {
     state.results.unshift(response);
     state.sequence = state.sequence + 1;
